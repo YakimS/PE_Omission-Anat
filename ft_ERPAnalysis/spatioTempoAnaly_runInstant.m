3f4be2ac9b8ab8f591381cedc6576f9abfd176d2
mashebali <ykmsharon@gmail.com>
2023-05-09 16:00:32 +0100

    add clusters dirs and running mats

--- ft_ERPAnalysis/spatioTempoAnaly_runInstant.m
+++ ft_ERPAnalysis/spatioTempoAnaly_runInstant.m
@@ -5,14 +5,25 @@ args.bl = 100;
 args.contrasts = {{'OFFirst5','ORFirst5'}};%{{'OFFirst5','ORFirst5'},{'OEf6','ORf6'},{'OF','OR'},{'OEf6','OR'}};
 args.pre_vs_post_conds_names = {'OFFirst5'};%{'OEf5','OEf6','OR','ORf6','OFFirst5','ORFirst5','T','O'};
 
+%% local variables
 args.ft_cond_dir= sprintf('C:\\Users\\User\\Cloud-Drive\\BigFiles\\OmissionExpOutput\\ft_erpAnalysis\\data_in_ft_cond_fomat');
 args.output_main_dir = sprintf('C:\\Users\\User\\Cloud-Drive\\BigFiles\\OmissionExpOutput\\ft_erpAnalysis\\spatiotemp_clusterPerm');
 args.libs_dir = sprintf('C:\\Users\\User\\Cloud-Drive\\BigFiles\\libs');
 args.code_dir = sprintf('C:\\Users\\User\\OneDrive\\Documents\\githubProjects\\PE_Omission-Anat\\ft_ERPAnalysis');
-
-args.wake_files_name_suffix = 'wake_all_referenced'; %wake_morning_referenced, wake_night_referenced, wake_all_referenced
 save(sprintf("%s\\spatioTempoAnaly_run_all.mat",args.code_dir),"args",'-mat')
 args.wake_files_name_suffix = 'wake_morning_referenced'; %wake_morning_referenced, wake_night_referenced, wake_all_referenced
 save(sprintf("%s\\spatioTempoAnaly_run_morning.mat",args.code_dir),"args",'-mat')
 args.wake_files_name_suffix = 'wake_night_referenced'; %wake_morning_referenced, wake_night_referenced, wake_all_referenced
 save(sprintf("%s\\spatioTempoAnaly_run_night.mat",args.code_dir),"args",'-mat')
+
+%% cluster variables
+args.ft_cond_dir= sprintf('//sci//labs//arzilab//ykmsharon//OmissionProject//OmissionSleepData//data_in_ft_cond_fomat');
+args.output_main_dir = sprintf('//sci//labs//arzilab//ykmsharon//OmissionProject//AnalysisResults//ft_ERPAnalysis');
+args.libs_dir = sprintf('//sci//labs//arzilab//ykmsharon//OmissionProject//Code//libs');
+args.code_dir = sprintf('//sci//labs//arzilab//ykmsharon//OmissionProject//Code//PE_Omission-Anat//ft_ERPAnalysis');
+args.wake_files_name_suffix = 'wake_all_referenced'; %wake_morning_referenced, wake_night_referenced, wake_all_referenced
+save(sprintf("clust_spatioTempoAnaly_run_all.mat"),"args",'-mat')
+args.wake_files_name_suffix = 'wake_morning_referenced'; %wake_morning_referenced, wake_night_referenced, wake_all_referenced
+save(sprintf("clust_spatioTempoAnaly_run_morning.mat"),"args",'-mat')
+args.wake_files_name_suffix = 'wake_night_referenced'; %wake_morning_referenced, wake_night_referenced, wake_all_referenced
+save(sprintf("clust_spatioTempoAnaly_run_night.mat"),"args",'-mat')
